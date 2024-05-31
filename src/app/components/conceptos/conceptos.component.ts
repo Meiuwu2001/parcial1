9d1e5dae5a6ae841c773eb667d2397ffb367988e
Meiuwu2001 <samuel.reyna434@gmail.com>
2024-05-30 22:15:05 -0600

    componentes

---
+++ src/app/components/conceptos/conceptos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conceptos',
+  standalone: true,
+  imports: [],
+  templateUrl: './conceptos.component.html',
+  styleUrl: './conceptos.component.css'
+})
+export class ConceptosComponent {
+
+}
---
+++ src/app/components/diagrama/diagrama.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagrama',
+  standalone: true,
+  imports: [],
+  templateUrl: './diagrama.component.html',
+  styleUrl: './diagrama.component.css'
+})
+export class DiagramaComponent {
+
+}
